280cb820fa1ee76bc63981e99f65c4c0a3eb8366
Sakshi03 <sakshimateer@gmail.com>
2018-02-04 17:16:50 +0530

    Calculator using react js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       
         <p className="App-intro">
           <h3>CALCULATOR</h3>
-
           <Welcome/>
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom';
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
