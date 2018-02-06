16a7732b0e46f1b5f2fda82dd5a134da1774cbdc
Sakshi03 <sakshimateer@gmail.com>
2018-02-06 23:35:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Welcome from './Welcome';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">
-          <h3>CALCULATOR</h3>
-          <Welcome/>
+          <span>CALCULATOR</span>
         </p>
+          <Welcome/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,158 +2,67 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Welcome extends Component{
-   
-    addValues(){
-        var no1= document.getElementById('textbox1');
-      var no2=document.getElementById('textbox2');
-      var mySum=document.getElementById('mySum');
-            var a = getNumberA();
-  var b = getNumberB();
-   var c = a + b;
-  postSumToPage(c);
-  function getNumberA() {
-  var a  = parseInt(no1.value);
-  return a;
-}
+constructor(props) {
+    super(props);
+    this.state = {value1: '', value2: '', result: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.addValues = this.addValues.bind(this);
 
-function getNumberB() {
-  var b = parseInt(no2.value);
-  return b;
-}
+  }
+ 
 
-function postSumToPage(c) {
-  var answer = c.toString();
-  mySum.innerHTML = "<h4>SUM:</h4>" + answer;
-}
-
-            }
-            subtractValues(){
-                var no1= document.getElementById('textbox1');
-      var no2=document.getElementById('textbox2');
-      var myDiff=document.getElementById('myDiff');
-            var a = getNumberA();
-  var b = getNumberB();
-   var d = a - b;
-  postDiffToPage(d);
-  function getNumberA() {
-  var a  = parseInt(no1.value);
-  return a;
-}
-
-function getNumberB() {
-  var b = parseInt(no2.value);
-  return b;
-}
-
-function postDiffToPage(d) {
-  var answer = d.toString();
-  myDiff.innerHTML = "<h4>DIFFERENCE:</h4>" + answer;
-}
-
-            }
-            multiplyValues(){
-                   var no1= document.getElementById('textbox1');
-      var no2=document.getElementById('textbox2');
-      var myProd=document.getElementById('myProd');
-            var a = getNumberA();
-  var b = getNumberB();
-   var c = a*b;
-  postProdToPage(c);
-  function getNumberA() {
-  var a  = parseInt(no1.value);
-  return a;
-}
-
-function getNumberB() {
-  var b = parseInt(no2.value);
-  return b;
-}
-
-function postProdToPage(c) {
-  var answer = c.toString();
-  myProd.innerHTML = "<h4>MULTIPLY:</h4>" + answer;
-}
-
-            }
-            divideValues(){
-                          var no1= document.getElementById('textbox1');
-      var no2=document.getElementById('textbox2');
-      var myDiv=document.getElementById('myDiv');
-            var a = getNumberA();
-  var b = getNumberB();
-   var c = a/b;
-  postDivToPage(c);
-  function getNumberA() {
-  var a  = parseInt(no1.value);
-  return a;
-}
-
-function getNumberB() {
-  var b = parseInt(no2.value);
-  return b;
-}
-
-function postDivToPage(c) {
-  var answer = c.toString();
-  myDiv.innerHTML = "<h4>DIVISION:</h4>" + answer;
-}
-            }
-             resetValues(){
-                 var mySum=document.getElementById('mySum');
-                 var myDiff=document.getElementById('myDiff');
-                 var myProd=document.getElementById('myProd');
-                 var myDiv=document.getElementById('myDiv');
-                          
-       var  reset = document.getElementById('reset');
-        document.getElementById('textbox1').value="";
-        document.getElementById('textbox2').value="";
-        mySum.innerHTML = '';
-        myDiff.innerHTML='';
-        myProd.innerHTML='';
-        myDiv.innerHTML='';
+  handleChange(valueType, event) {
+    if(valueType === "firstValue"){
+      this.setState({value1: event.target.value});
+    } else{
+      this.setState({value2: event.target.value});
     }
-    
-    render(){
-        const name = "Sakshi";
-        return (
-            
-         
-    <form className="Calculate">
+    console.log(this.state)
+  }
+
+  addValues(value1, value2) {
+    this.setState({result: parseInt(value1) + parseInt(value2)});
+    console.log(this.state.result)
+  }
+  
+  subtractValues(value1, value2) {
+    this.setState({result: parseInt(value1) - parseInt(value2)});
+    console.log(this.state.result)
+  }
+  
+  multiplyValues(value1, value2) {
+    this.setState({result: parseInt(value1) * parseInt(value2)});
+    console.log(this.state.result)
+  }
+  
+  divideValues(value1, value2) {
+    this.setState({result: parseInt(value1) / parseInt(value2)});
+    console.log(this.state.result)
+  }
+render() {
+    return (
+      <form>
         <label>
-          First Value
-          <input type="number" id="textbox1"/>
+          First Value:
+          <input type="number" value={this.state.value1} onChange={(e)=>{this.handleChange("firstValue", e)}} />
+        </label><br/><br/>
+        <label>
+          Second Value:
+          <input type="number" value={this.state.value2} onChange={(e)=>{this.handleChange("secondValue", e)}} />
+        </label><br/><br/>
+
+        <input type="submit" value="Add" onClick={(e)=>{e.preventDefault();this.addValues(this.state.value1, this.state.value2)}} />
+        <input type="submit" value="Subtract" onClick={(e)=>{e.preventDefault();this.subtractValues(this.state.value1, this.state.value2)}} />
+        <input type="submit" value="Multiply" onClick={(e)=>{e.preventDefault();this.multiplyValues(this.state.value1, this.state.value2)}} />
+        <input type="submit" value="Divide" onClick={(e)=>{e.preventDefault();this.divideValues(this.state.value1, this.state.value2)}} />
+
+        <br/><br/><label>RESULT
+          <input type="textbox" value={this.state.result}></input>
         </label>
-        <br/><br/>
-         <label>
-             Second Value
-          <input type="number" id="textbox2"/>
-        </label>
-        <br/><br/>
-        <div id="answer">
-       <div id="mySum">
-           </div>
-           <div id="myDiff">
-               </div>
-               <div id="myProd">
-                   </div>
-                   <div id="myDiv">
-                       </div>
-           </div>
-        
-        <br/><br/>
-        <input type="button" className="Add-button" value="Add" onClick={(e)=> {e.preventDefault(); this.addValues();}} />
-        <input type="button" value="Subtract" onClick={(e)=> {e.preventDefault(); this.subtractValues();}} />
-        <input type="button" value="Multiply"onClick={(e)=> {e.preventDefault(); this.multiplyValues();}} />
-        <input type="button" value="Divide" onClick={(e)=> {e.preventDefault(); this.divideValues();}}/>
-         <input type="reset" value="Reset" id="reset" onClick={(e)=> {e.preventDefault(); this.resetValues();}}/>
-        
       </form>
-
-            // <p>Welcome {this.props.name} {name}</p>
-        )   
-        
-    }
-
+    );
+  }
 }
+
 
 export default Welcome;
